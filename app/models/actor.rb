ef11dd7f5071f283d7d3b47871a430f3a84a9168
Matt Trussell <smtrussell@gmail.com>
2018-02-15 00:10:22 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full_name = self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    list_of_roles = []
+    character_arr = self.characters
+    character_arr.each do |character|
+      name_and_show = "#{character.name} - #{character.show.name}"
+      list_of_roles << name_and_show
+    end
+    list_of_roles
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(call_letters:)
+    @call_letters = call_letters
+    network = Network.new(:call_letters => @call_letters)
+    self.network = network
+    self.save
+  end
+
 end
